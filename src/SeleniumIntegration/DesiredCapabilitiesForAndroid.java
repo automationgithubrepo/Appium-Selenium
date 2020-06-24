12d623402e14aaecb1020b2302955837d21abe98
Dell <Dell@10.28.8.230>
2020-06-24 16:58:30 +0530

    Framework Design

--- src/SeleniumIntegration/DesiredCapabilitiesForAndroid.java
+++ src/SeleniumIntegration/DesiredCapabilitiesForAndroid.java
@@ -9,7 +9,6 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-
 import io.appium.java_client.android.AndroidDriver;
 
 public class DesiredCapabilitiesForAndroid {
