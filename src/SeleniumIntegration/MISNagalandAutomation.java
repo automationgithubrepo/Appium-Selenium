4f39438f1f1fc2167dc85dcb4f83f7a1a1df978e
Dell <Dell@10.28.8.191>
2020-06-29 17:56:58 +0530

    MISNaglandautomation

--- src/SeleniumIntegration/MISNagalandAutomation.java
+++ src/SeleniumIntegration/MISNagalandAutomation.java
@@ -1,21 +1,25 @@
 package SeleniumIntegration;
-
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
 
-public class WhatsAppAutomation 
+public class MISNagalandAutomation 
 {
-	public static AndroidDriver ad;
+	public static AndroidDriver ad ;
 	@BeforeTest
 	public static void LaunchApk() throws InterruptedException, MalformedURLException
 	{
@@ -23,33 +27,39 @@ public class WhatsAppAutomation
 		dc.setCapability("deviceName", "A");
 		dc.setCapability("platformVersion", "9");
 		dc.setCapability("platformName", "Android");
-		dc.setCapability("appWaitActivity", "com.whatsapp.HomeActivity");
-		dc.setCapability("appWaitPackage", "com.whatsapp");
+		dc.setCapability("appWaitActivity", "com.android.packageinstaller.permission.ui.GrantPermissionsActivity");
+		dc.setCapability("appWaitPackage", "com.google.android.packageinstaller");
 		dc.setCapability("adbExecTimeout", "20000");
 		dc.setCapability("appWaitDuration", "20000");
-		File f= new File("E:\\MobileWP\\Appium\\apk\\WhatsApp.apk");
+		File f= new File("C:\\Users\\Dell\\git\\Appium-Selenium\\apk\\MISNagaland.apk");
 		dc.setCapability("app", f.getAbsolutePath());
 		ad = new AndroidDriver<WebElement>(new URL("http://192.168.43.59:4723/wd/hub"), dc);
 		System.out.println("!!!Application Launched!!!");
 		Thread.sleep(2000);
 	}
 	@Test
-	public static void TextMessage() throws InterruptedException
+	public static void AllowPermissions() throws InterruptedException
 	{
-		ad.findElementByAccessibilityId("Search").click();
-		ad.findElement(By.id("com.whatsapp:id/search_src_text")).sendKeys("rinky");
-		ad.findElement(By.id("com.whatsapp:id/contact_row_container")).click();
-		ad.findElement(By.id("com.whatsapp:id/entry")).sendKeys("HI.....**This Message is through Automated Script**");
-		ad.findElementByAccessibilityId("Send").click();
+		ad.findElement(By.id("com.android.packageinstaller:id/permission_allow_button")).click();
+		Thread.sleep(2000);
+		ad.findElement(By.id("com.android.packageinstaller:id/permission_allow_button")).click();
+		Thread.sleep(3000);
+		ad.findElement(By.id("com.android.packageinstaller:id/permission_allow_button")).click();
 	}
 	@Test(priority=1)
-	public static void MakeACall() throws InterruptedException
+	public static void SubtractFunction() throws InterruptedException
 	{
-		
+
 	}
 	@Test(priority=2)
-	public static void AddStatus() throws InterruptedException
+	public static void Multi() throws InterruptedException
 	{
+
+	}
+	@Test(priority=3)
+	public static void Divide() throws InterruptedException
+	{
+
 
 	}
 	@AfterTest
@@ -58,8 +68,4 @@ public class WhatsAppAutomation
       ad.quit();
       System.out.println("!!!Test Executed Successfully!!!");
 	}
-	
-	
-	
-	
 }
