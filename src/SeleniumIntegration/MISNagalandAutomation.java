e5dd1a100211c9a8f44d155eeccfa68fbafabb91
Dell <Dell@10.28.8.165>
2020-06-30 17:18:57 +0530

    Profile Module updated

--- src/SeleniumIntegration/MISNagalandAutomation.java
+++ src/SeleniumIntegration/MISNagalandAutomation.java
@@ -12,7 +12,6 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-
 import Utilities.Screenshots;
 public class MISNagalandAutomation 
 {
